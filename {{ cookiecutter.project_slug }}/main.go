b8c28a619b652d9be498d90eb94ab677412b90cb
Darryl Balderas <10657206+darrylbalderas@users.noreply.github.com>
2024-02-26 20:11:55 -0800

    Update main.go

--- {{ cookiecutter.project_slug }}/main.go
+++ {{ cookiecutter.project_slug }}/main.go
@@ -7,28 +7,20 @@ import (
 	"net/http"
 )
 
-// ResponseData struct represents the structure of the response data
 type ResponseData struct {
 	Message string `json:"message"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Set the content type header to indicate the type of data being returned
 	w.Header().Set("Content-Type", "application/json")
-
-	// Create a ResponseData object with required data
 	response := ResponseData{
 		Message: "Hello world",
 	}
-
-	// Marshal the response data into JSON format
 	jsonResponse, err := json.Marshal(response)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	// Write the JSON response to the http.ResponseWriter
 	w.Write(jsonResponse)
 }
 
